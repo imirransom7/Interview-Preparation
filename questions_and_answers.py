9227d8c5dbee3796f856e05a8baf2bb768d34483
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-08-30 09:04:15 -0400

    added in some more questions and answers for the technical

---
+++ questions_and_answers.py
@@ -0,0 +1,71 @@
+technical_questions = {
+    'id': [0, 1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20],
+    'question': [
+        'What is Polymorphism?',
+        'What is a fibonacci series?',
+        'What is a Join in SQL?',
+        'What are all the Joins in SQL?',
+        'What is an Inner Join?',
+        'What is Left Join?',
+        'What is Right Join?',
+        'What is Full Join?',
+        'Explain the process of a user visiting a web page. What HTTP methods are involved?',
+        'What are your thoughts on declarative vs. imperative paradigms?',
+        'What is OOP?'
+        'What are the 4 pillars of OOP?'
+        'What is Abstraction?'
+        'What is Inheritance?'
+        'What is Encapsulation?'
+        'What is functional programming?'
+        'What are you thoughts functional and object-oriented programming?'
+        'What are list?',
+        'What are Tuples?',
+        'What is a dictionary?',
+        'What is a set?',
+    ],
+    'answer': [
+        'many forms;  methods/functions/operators with the same name that can be executed on many objects or classes\
+        allows multiple classes to implement the same method in different ways',
+        'a sequence in which each number is the sum of the two preceding ones',
+        'a clause used to combine rows from two or more tables based on a related column between them',
+        'inner Join, Left Join, Right Join, Full Join',
+        'returns all data that have matching values in both tables',
+        'returns all data from the left table, and the matched data from the right table',
+        'returns all data from the right table, and the matched data from the left table',
+        'returns all data when there is a match in either the left or right',
+        'user enters URL in browser. This sends a request to a DNS server to translate the domain name into an IP\
+        Address. The browser then connects to that IP address, request webpage data, receives the HTML code from the\
+        web server, and finally renders the page on the screen, displaying the website content. primarily involves the\
+        GET HTTP method, which is used to retrieve data from a server without modifying it.',
+        'declarative programming generally leads to more readable, maintainable, and easier-to-understand code; it is\
+        code that says what you want; what the program should do, while imperative code says how you want something\
+        done; how the program should do it. this is good to use when your problem involves complex control flows and\
+        state changes,\
+        or when you need to interact with low-level hardware',
+        'it is Object Oriented Programming, a programming paradigm that organizes software design around objects,\
+        rather than functions and logic',
+        'the 4 pillars of OOP are Abstract, Inheritance, Encapsulation, and Polymorphism',
+        'the process of simplifying code by removing unnecessary details and hiding complexity, This makes it easier to\
+        implement algorithms and communicate with digital tools.',
+        'allows programmers to create new classes based on existing ones;  creates a hierarchy of classes that share\
+        methods and attributes',
+        'bundle data and methods into easy-to-use units; such as classes. Encapsulation is also used in scope.\
+        Encapsulates things that should not be modified by other parts of your code',
+        'using functions as data objects throughout code regardless of whether the language makes this simple or not.\
+        An approach to software development that uses pure functions to create maintainable software',
+        'OOP is a good choice to use if  memory is limited, as FP tends to require more memory. on the other hand,\
+        Functional programming makes it so that all of your objects are (at least semantically) immutable and that\
+        every function is referentially transparent',
+        'a data structure that is in mutable that can hold any time of data type or structure inside them. It has many\
+        various built-in methods for manipulation. However, lists consumes more memory and has slower iteration but is\
+        better for insertion and deletion',
+        'a data type that is immutable and consumes less memory. Has faster iteration, but is not as flexible with\
+        limited built-in functions',
+        'a mutable data structure that allows you to store key-value pairs; a collection which is ordered (Python 3.6\
+        and earlier, dictionaries are unordered), changeable and do not allow duplicates for the keys',
+        'a data collection type used in Python for storing multiple items in a single variable; sets are unordered.\
+        These are generally used when you do not want a repeat of data',
+
+
+    ]
+}
