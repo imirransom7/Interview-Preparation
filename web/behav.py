6893c332f911b9e47ff72c45d6b70839a4ecd064
Imir Ransom <122006390+imirransom7@users.noreply.github.com>
2024-09-02 10:48:42 -0400

    added blueprint for behavioral questions

---
+++ web/behav.py
@@ -0,0 +1,8 @@
+from flask import Blueprint, render_template
+from questions_data.behavioral_questions import behavioral_questions
+
+behav = Blueprint('behav', __name__)
+
+@behav.route('/behav')
+def behavioral():
+    render_template('behav.html', questions=behavioral_questions['question'])
